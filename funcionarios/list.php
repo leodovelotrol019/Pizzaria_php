1b1782d67039dccfe984b49d29d5c91b7c4c8518
L3O019 <dadu.tuber@gmail.com>
2026-02-12 15:58:24 -0300

    new add

---
+++ funcionarios/list.php
@@ -0,0 +1,77 @@
+    <?php
+//conexao do PHP com o banco de dados MYSQL
+ define('SERVIDOR','localhost' );
+ define('USUARIO','root');
+ define('SENHA','');
+ define('BANCO','db_pizzaria');
+
+// string de conexao usando PDO
+try{
+$conexao = new PDO("mysql:host=".SERVIDOR.";dbname=".BANCO.";charset=utf8",USUARIO,SENHA);
+
+$conexao->setATTribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+
+}catch(PDOExeption $erro){
+    echo"Erro ao conectar no banco de dados ".$erro;
+}
+$sql = "select * from tb_funcionario";
+$comando = $conexao->prepare($sql);
+$comando->execute();
+$funcionarios = $comando ->fetchALL(PDO::FETCH_ASSOC);
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
+    <title>LISTA DE FUNCIONARIOS</title>
+</head>
+<body>
+   <header>
+    <h1>
+        LISTA DE FUNCIONARIOS
+    </h1>
+   </header>
+
+   <main>
+<div id="container">
+<table>
+    <tr>
+        <th>ID</th>
+        <th>NOME</th>
+        <th>E-MAIl</th>
+        <th>TELEFONE</th>
+    </tr>
+    <?php foreach ($funcionarios as $funcionario):?>
+    <tr>
+        <td><?php echo $funcionario ['id'];?></td>
+        <td><?php echo $funcionario ['nome'];?></td>
+        <td><?php echo $funcionario ['email'];?></td>
+        <td><?php echo $funcionario ['telefone'];?></td>
+    </tr>
+    <?php endforeach; ?>
+</table>
+</div>
+
+   </main>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ html/pizza-lista.php
@@ -0,0 +1,78 @@
+    <?php
+//conexao do PHP com o banco de dados MYSQL
+ define('SERVIDOR','localhost' );
+ define('USUARIO','root');
+ define('SENHA','');
+ define('BANCO','db_pizzaria');
+
+// string de conexao usando PDO
+try{
+$conexao = new PDO("mysql:host=".SERVIDOR.";dbname=".BANCO.";charset=utf8",USUARIO,SENHA);
+
+$conexao->setATTribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+
+}catch(PDOExeption $erro){
+    echo"Erro ao conectar no banco de dados ".$erro;
+}
+$sql = "select * from tb_funcionario";
+$comando = $conexao->prepare($sql);
+$comando->execute();
+$pizzas = $comando->fetchALL(PDO::FETCH_ASSOC);
+// echo"<pre>";
+// var_dump($pizzas);
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pizzaria - listagem</title>
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+
+<body>
+    <header>
+        <h1>
+            Pizzaria - Listagem
+        </h1>
+    </header>
+    <main>
+        <div id="container">
+            <table >
+                <tr>
+                    <th>
+                        id
+                    </th>
+                    <th>
+                        Sabor
+                    </th>
+                    <th>
+                        Ingrediente
+                    </th>
+                    <th>
+                        Tamanho
+                    </th>
+                    <th>Valor</th>
+                </tr>
+                <?php
+                foreach ($pizzas as $pizza):
+                    
+                ?>
+<tr>
+    <td><?php echo $pizza ['id'];?></td>
+    <td><?php echo $pizza ['sabor'];?></td>
+    <td><?php echo $pizza ['ingrediente'];?></td>
+    <td><?php echo $pizza ['tamanho'];?></td>
+    <td><?php echo $pizza ['valor'];?></td>
+</tr>                    
+<?php endforeach;?>
+
+            </table>
+        </div>
+    </main>
+</body>
+
+</html>
